e523c9aec454bc43e787cfebb3486da506123795
smistik <smistik@stredniskola.com>
2024-06-07 08:31:05 +0200

    Inicializován generator nahodných čísel

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "iostream"
 #include "color.h"
+#include "ctime"
 
 /*Hadani tajneho cisla*/
 using namespace std;
 int main(int argc, char *argv[]){
 	system("chcp 65001>nul");
+	srand(time(0));
 	int tajneCislo;
 	int hracovoCislo;
 	tajneCislo = rand()%5+1; // nahodne od 1 do 5
